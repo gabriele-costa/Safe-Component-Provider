3f7251f6fbf97fe735a8a5622053e6337d7ec409
SCPTeam <yugo.amaril@gmail.com>
2015-04-02 17:14:16 +0200

    * Uncommented *

--- SCPcore/jni/dbHandler.h
+++
@@ -1,9 +0,0 @@
-#include "sqlite/sqlite3.h"
-#include "constants.h"
-#include "componentsArray.h"
-
-int openDb(sqlite3 **db);
-int insertComponent(t_component *tc);
-int removeComponents(const char *pack);
-static int callback(void *data, int argc, char **argv, char **azColName);
-void selectComponent(const char *componentName, t_componentsArray *);
--- SCPcore/jni/miniSat/core/Launch.h
+++
@@ -1,6 +0,0 @@
-#ifndef Launch_h
-#define Launch_h
-
-char* minisat(const char* in, bool debug);
-
-#endif
